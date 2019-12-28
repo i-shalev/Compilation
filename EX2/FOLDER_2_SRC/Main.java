4367b0d41995e61f706545376b8991b453defe77
yahav <yahavxx@gmail.com>
2019-12-28 04:06:37 +0200

    added tester to ex2 disabled all debug prints fixed ex2 expected outputs

--- EX2/FOLDER_2_SRC/Main.java
+++ EX2/FOLDER_2_SRC/Main.java
@@ -17,8 +17,8 @@ public class Main
 		String inputFilename = argv[0];
 		String outputFilename = argv[1];
 
-		boolean printDerivationRule = true;
-		boolean printTokens = true;
+		boolean printDerivationRule = false;
+		boolean printTokens = false;
 
 		try
 		{
@@ -31,7 +31,7 @@ public class Main
 			AST.PrintMe();
 			file_writer.println("OK");
 			file_writer.close();
-			AST_GRAPHVIZ.getInstance().finalizeFile(); // Creates AST graphics
+			AST_Graphviz.getInstance().finalizeFile(); // Creates AST graphics
     	}
 			     
 		catch (Exception e)
