16e0a0a08570ea55d6003f441acd8d13d1bc56df
Allen <allenchikman@gmail.com>
2020-01-07 10:28:18 +0200

    remove redundunt comments - in the main

--- EX4/FOLDER_2_SRC/Main.java
+++ EX4/FOLDER_2_SRC/Main.java
@@ -1,92 +1,40 @@
-   
+
 import java.io.*;
 import java.io.PrintWriter;
+
 import java_cup.runtime.Symbol;
 import AST.*;
 import IR.*;
 import MIPS.*;
 
-public class Main
-{
-	static public void main(String argv[])
-	{
-		Lexer l;
-		Parser p;
-		Symbol s;
-		AST_DEC_LIST AST;
-		FileReader file_reader;
-		PrintWriter file_writer;
-		String inputFilename = argv[0];
-		String outputFilename = argv[1];
-		
-		try
-		{
-			/********************************/
-			/* [1] Initialize a file reader */
-			/********************************/
-			file_reader = new FileReader(inputFilename);
+public class Main {
+    static public void main(String argv[]) {
+        Lexer l;
+        Parser p;
+        Symbol s;
+        AST_DEC_LIST AST;
+        FileReader file_reader;
+        PrintWriter file_writer;
+        String inputFilename = argv[0];
+        String outputFilename = argv[1];
 
-			/********************************/
-			/* [2] Initialize a file writer */
-			/********************************/
-			file_writer = new PrintWriter(outputFilename);
-			
-			/******************************/
-			/* [3] Initialize a new lexer */
-			/******************************/
-			l = new Lexer(file_reader);
-			
-			/*******************************/
-			/* [4] Initialize a new parser */
-			/*******************************/
-			p = new Parser(l);
-
-			/***********************************/
-			/* [5] 3 ... 2 ... 1 ... Parse !!! */
-			/***********************************/
-			AST = (AST_DEC_LIST) p.parse().value;
-			
-			/*************************/
-			/* [6] Print the AST ... */
-			/*************************/
-			AST.PrintMe();
-
-			/**************************/
-			/* [7] Semant the AST ... */
-			/**************************/
-			AST.SemantMe();
-
-			/**********************/
-			/* [8] IR the AST ... */
-			/**********************/
-			AST.IRme();
-			
-			/***********************/
-			/* [9] MIPS the IR ... */
-			/***********************/
-			IR.getInstance().MIPSme();
-
-			/**************************************/
-			/* [10] Finalize AST GRAPHIZ DOT file */
-			/**************************************/
-			AST_GRAPHVIZ.getInstance().finalizeFile();			
-
-			/***************************/
-			/* [11] Finalize MIPS file */
-			/***************************/
-			sir_MIPS_a_lot.getInstance().finalizeFile();			
-
-			/**************************/
-			/* [12] Close output file */
-			/**************************/
-			file_writer.close();
-    	}
-			     
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+        try {
+            file_reader = new FileReader(inputFilename);
+            file_writer = new PrintWriter(outputFilename);
+            l = new Lexer(file_reader);
+            p = new Parser(l);
+            AST = (AST_DEC_LIST) p.parse().value;
+            AST.PrintMe();
+            AST.SemantMe();
+            AST.IRme();
+            IR.getInstance().MIPSme();
+            AST_GRAPHVIZ.getInstance().finalizeFile();
+            sir_MIPS_a_lot.getInstance().finalizeFile();
+            file_writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
