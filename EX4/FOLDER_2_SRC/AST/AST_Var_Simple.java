651fad43a0f09fc433f418315c52ef8c0a8a0e0e
itay <itayshalev2@gmail.com>
2020-01-11 19:16:43 +0200

    change SemantMe to Var_Simple

--- EX4/FOLDER_2_SRC/AST/AST_Var_Simple.java
+++ EX4/FOLDER_2_SRC/AST/AST_Var_Simple.java
@@ -1,13 +1,20 @@
 package AST;
 
 import SYMBOL_TABLE.SymbolTable;
+import TYPES.Symbol;
 import TYPES.Type;
 import TYPES.Type_Class;
 import TYPES.Type_Scope;
 
+import java.util.List;
+
 public class AST_Var_Simple extends AST_Var
 {
     public String name;
+    public int local = -1;
+    public int param = -1;
+    public int member = -1;
+
 
     public AST_Var_Simple(String name)
     {
@@ -23,6 +30,38 @@ public class AST_Var_Simple extends AST_Var
     }
 
     public Type SemantMe() throws Exception{
+
+        if (SymbolTable.findFunc() != null) {
+            for (int i = SymbolTable.findFunc().currMaxLocals - 1; i >= 0; i--)
+            {
+                if (name.equals(SymbolTable.findFunc().locals.get(i).name))
+                {
+                    local = i;
+                    break;
+                }
+            }
+            List<Symbol> funcParams = SymbolTable.findFunc().params2;
+            for (int i = 0; i < funcParams.size(); i++)
+            {
+                if (name.equals(funcParams.get(i).name))
+                {
+                    param = i;
+                }
+            }
+        }
+
+        if(SymbolTable.findClass() != null)
+        {
+            List<Symbol> classMembers = SymbolTable.findClass().members;
+            for (int i = 0; i < classMembers.size(); i++)
+            {
+                if (name.equals(classMembers.get(i).name))
+                {
+                    member = i;
+                }
+            }
+        }
+
         Type scopeVar = SymbolTable.findInScope(name);
 
         // First check the current scope
